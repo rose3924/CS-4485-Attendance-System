8c6c7369d3c2c39bd16a772807bf065f7b7b93fb
rose3924 <oliviarose392@gmail.com>
2025-04-04 00:53:46 -0500

    Updated quiz display and login page

--- StudentQuiz/Program.cs
+++ StudentQuiz/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddServerSideBlazor();
 
 ///////////////////////////////////////////////////////////////
 /// Settings for the database
